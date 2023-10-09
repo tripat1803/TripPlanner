ec5494b7bb5c9a462e9e0d5eee3c4594540eba3d
yashikaa05 <yashika1585.be21@chitkara.edu.in>
2023-10-09 21:01:10 +0530

    <Frist commit>

---
+++ src/components/Home/Features.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import "../../styles/features.css";
+
+function FeaturesCard({icon, heading, body}) {
+    return (
+        <div className="layout">
+            <div className="icon">{icon}</div>
+            <div className="feature">
+                <h4>{heading}</h4>
+                <p>{body}</p>
+            </div>
+        </div>
+    )
+}
+
+export default function Features() {
+    return (
+        <div className="container">
+            <div className="container-1">
+                <h6>TRAVELUX.AI</h6>
+                <h3>The only tool you'll ever need!</h3>
+                <p>Say goodbye to the stress of planning and hello to personalized recommendations, efficient itineraries, and seamless dining experiences.</p>
+            </div>
+
+            <div className="container-3">
+                <div className="row">
+                    <FeaturesCard icon="🗺️" heading="Optimal Route Planning" body="Our AI algorithms analyze your preferences to craft the most efficient route, saving you time and effort." />
+                    <FeaturesCard icon="🧑‍💻" heading="Personalize Your Adventure" body="Shape your journey by freely adding, editing, or deleting activities from your itinerary." />
+                </div>
+
+                <div className="row">
+                    <FeaturesCard icon="📲" heading = "Travel Reel Insights" body="Utilize social media's potential by gathering travel insights from Instagram reels and integrate them into your itinerary."/>
+                    <FeaturesCard icon="🦐" heading="Local Cuisine Recommendations" body="Discover local cuisines and hidden gems recommended by our AI, tailored to your taste buds."/>
+
+                </div>
+            </div>
+        </div>
+    )
+}
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,14 @@
 import React, { Fragment } from 'react';
 import Hero from '../components/Home/Hero';
+import Testimonials from '../components/Home/Testimonials';
+import Features from '../components/Home/Features';
 
 export default function Home() {
   return (
     <Fragment>
       <Hero/>
+      <Features/>
+      <Testimonials/>
     </Fragment>
   );
 }
