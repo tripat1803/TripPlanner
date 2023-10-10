39213d8c4efeb8961a67f81389ef5804b171f5a7
tripat1803 <tripatjot1455.be21@chitkara.edu.in>
2023-10-10 09:46:08 +0530

    Created hotels route

---
+++ src/pages/Hotels.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Layout from '../components/Trip/Layout'
+
+export default function Hotels() {
+  return (
+    <Layout>
+        
+    </Layout>
+  )
+}
--- src/routes/TripRoutes.js
+++ src/routes/TripRoutes.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route, Routes, useParams } from 'react-router-dom';
 import Trip from '../pages/Trip';
 import Meals from '../pages/Meals';
+import Hotels from '../pages/Hotels';
 
 export default function TripRoutes() {
     
@@ -11,6 +12,7 @@ export default function TripRoutes() {
         <Routes>
             <Route path='/' element={<Trip />} />
             <Route path='/meals' element={<Meals/>} />
+            <Route path='/hotels' element={<Hotels/>} />
         </Routes>
     )
 }
