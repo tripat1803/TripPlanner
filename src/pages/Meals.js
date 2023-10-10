808fed3a8fc8085b15a401409ce0c54e531397a3
yashikaa05 <yashika1585.be21@chitkara.edu.in>
2023-10-10 15:14:20 +0530

    Started meals section

--- src/pages/Meals.js
+++ src/pages/Meals.js
@@ -1,12 +1,32 @@
-import React from 'react'
-import Layout from '../components/Trip/Layout'
+import React from 'react';
+import Layout from '../components/Trip/Layout';
+
+const neutralsGray04 = "text-[#999999]";
 
 export default function Meals() {
   return (
     <Layout>
         <div className='flex flex-col gap-3'>
             <h1 className='text-4xl'>Meals preference</h1>
-            <p className='text-[#999999]'>Select the meals you would like to include in your trip, your budget for it and the type of food you prefer.</p>
+            <p className={neutralsGray04}>Select the meals you would like to include in your trip, your budget for it and the type of food you prefer.</p>
+        </div>
+
+        <div className='flex flex-col gap-[6px]'>
+            <h2>Budget level:</h2>
+            <p className={neutralsGray04}>Depending on the budget level, we will select least or more expensive restaurants.</p>
+            <input type='range' id='meal-budget'></input>
+
+        </div>
+
+        <div className='flex flex-col gap-3'>
+          <h2>Meals to include:</h2>
+          <p className={neutralsGray04}>Prices are an estimate of each meal for 8 days per 2 people</p>
+          <input type='checkbox' />
+          <div className='heading'>
+            <h2>Breakfast</h2>
+            <p>($2 per meal)</p>
+          </div>
+
         </div>
 
     </Layout>
