5b460bed08917b444b8820f8f2fbc05917bf7ca7
tripat1803 <tripatjot1455.be21@chitkara.edu.in>
2023-10-10 15:13:51 +0530

    Merged conflict files

--- src/App.js
+++ src/App.js
@@ -1,19 +1,6 @@
 import React, { Fragment } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Authentication from './pages/Authentication';
-<<<<<<< HEAD
-import MainPage from './pages/MainPage';
-
-const App = () => {
-  return (
-    <>
-    <MainPage />
-    {/* <Routes>
-      <Route path='/' element={<Authentication/>} />
-    </Routes> */}
-    </>
-  )
-=======
 import Home from './pages/Home';
 import Navbar from './components/Navbar';
 import TripRoutes from './routes/TripRoutes';
@@ -29,7 +16,6 @@ const App = () => {
       </Routes>
     </Fragment>
   );
->>>>>>> 39213d8c4efeb8961a67f81389ef5804b171f5a7
 }
 
 export default App;
