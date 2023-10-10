541345c838b9af40b9bee0dce06d86fa451cadf5
yashikaa05 <yashika1585.be21@chitkara.edu.in>
2023-10-10 16:11:00 +0530

    Meal section 80% completed

--- src/pages/Meals.js
+++ src/pages/Meals.js
@@ -3,6 +3,21 @@ import Layout from '../components/Trip/Layout';
 
 const neutralsGray04 = "text-[#999999]";
 
+function MealsOption({meal, cost, totalCost}){
+  return(
+          <div className='flex gap-5 items-center'>
+            <input type='checkbox' id='breakfast' />
+            <label htmlFor='breakfast'>
+              <h2 className='text-lg'>{meal}</h2> 
+              <p className={'text-xs ' + neutralsGray04}>({cost} per meal)</p>
+            </label>
+            <p>-</p>
+            <h3 className={'text-lg ' + neutralsGray04}>₹{totalCost}</h3>
+          </div>
+    
+  )
+}
+
 export default function Meals() {
   return (
     <Layout>
@@ -19,14 +34,13 @@ export default function Meals() {
         </div>
 
         <div className='flex flex-col gap-3'>
-          <h2>Meals to include:</h2>
-          <p className={neutralsGray04}>Prices are an estimate of each meal for 8 days per 2 people</p>
-          <input type='checkbox' />
-          <div className='heading'>
-            <h2>Breakfast</h2>
-            <p>($2 per meal)</p>
+          <div>
+            <h2>Meals to include:</h2>
+            <p className={neutralsGray04}>Prices are an estimate of each meal for 8 days per 2 people</p>
           </div>
-
+          <MealsOption meal="Breakfast" cost="₹60" totalCost="200"/>
+          <MealsOption meal="Lunch" cost="₹80" totalCost="200"/>
+          <MealsOption meal="Dinner" cost="₹60" totalCost="200"/>
         </div>
 
     </Layout>
