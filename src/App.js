4605b875478818f5ae82b263f43a0248358a0353
tripat1803 <tripatjot1455.be21@chitkara.edu.in>
2023-10-25 18:45:56 +0530

    Added footer to app.js

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Navbar from './components/Navbar';
 import TripRoutes from './routes/TripRoutes';
 import { Toaster } from 'react-hot-toast';
 import Chatbot from './components/Chatbot';
+import Footer from './components/Footer';
 
 const App = () => {
   return (
@@ -16,6 +17,7 @@ const App = () => {
         <Route path='/auth' element={<Authentication/>} />
         <Route path='/:id/trip/*' element={<TripRoutes/>} />
       </Routes>
+      <Footer/>
       <Chatbot/>
       <Toaster/>
     </Fragment>
