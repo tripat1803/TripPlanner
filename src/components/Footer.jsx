4b9c61f2ae1aa101cb004bbfdd78ea7b8a2bf49e
yashikaa05 <yashika1585.be21@chitkara.edu.in>
2023-10-26 10:54:29 +0530

    Footer responsiveness

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -12,41 +12,43 @@ function FooterLink({ text }) {
 
 export default function Footer() {
     return (
-        <div className='flex flex-col gap-10 px-[276px] py-[160px] bg-[#d9d9d9] relative'>
+        <div className='flex justify-center px-8 sm:px-16 py-20 bg-[#d9d9d9] relative'>
             <img className='absolute top-0 left-0 w-full h-full object-cover z-[1]' src={require("../assets/background.png")} />
             <div className='absolute top-0 left-0 w-full h-full bg-[#2F6A7F] mix-blend-hard-light z-[2]'></div>
-            <div className='z-[13] flex flex-col gap-4'>
-                <h1 className='text-[54px]	text-white font-semibold'>Looking for something else?</h1>
-                <div className='flex justify-between'>
-                    <div className='text-lg text-white'>
-                        <p>Connect with our experts!</p>
-                        <p>Get the best itineraries and offers!</p>
+            <div className='flex flex-col gap-10 xl:w-[75%]'>
+                <div className='z-[13] flex flex-col gap-4'>
+                    <h1 className='text-[38px] md:text-[44px] lg:text-[54px]	text-white font-semibold'>Looking for something else?</h1>
+                    <div className='flex justify-between'>
+                        <div className='text-lg text-white'>
+                            <p>Connect with our experts!</p>
+                            <p>Get the best itineraries and offers!</p>
+                        </div>
+                        <button className='py-0.5 px-8 rounded-full font-medium text-lg text-white bg-[linear-gradient(90deg,#10B5CB_0%,#0073A8_100%)]'>Connect us</button>
                     </div>
-                    <button className='py-0.5 px-8 rounded-full font-medium text-lg text-white bg-[linear-gradient(90deg,#10B5CB_0%,#0073A8_100%)]'>Connect us</button>
                 </div>
-            </div>
 
-            <div className='z-[13] overflow-hidden rounded-[12px]'>
-                <div className='flex justify-between items-end px-10 py-6 bg-white'>
-                    <div className='flex flex-col gap-[4px]'>
-                        <h1 className='text-[24px] font-semibold'>Travelux.ai</h1>
-                        <p className='w-[320px] text-[#666666]'>Turn your next trip into a hassle-free experience with Trip Planner AI.</p>
-                        <FooterLink text={"Start Planning"} />
+                <div className='z-[13] overflow-hidden rounded-[12px]'>
+                    <div className='flex justify-between items-end px-10 py-6 bg-white'>
+                        <div className='flex flex-col gap-[4px]'>
+                            <h1 className='text-[24px] font-semibold'>Travelux.ai</h1>
+                            <p className='w-[320px] text-[#666666]'>Turn your next trip into a hassle-free experience with Trip Planner AI.</p>
+                            <FooterLink text={"Start Planning"} />
+                        </div>
+                        <div className='flex flex-col gap-[4px]'>
+                            <h3 className='font-semibold text-[#666666] text-lg'>QUICK LINKS</h3>
+                            <FooterLink text={"Privacy Policy"} />
+                            <FooterLink text={"Disclaimer"} />
+                        </div>
+                        <div className='flex flex-col gap-[4px]'>
+                            <FooterLink text={"Our team"} />
+                            <FooterLink text={"Cancellation Policy"} />
+                            <FooterLink text={"Privacy Policyr"} />
+                        </div>
                     </div>
-                    <div className='flex flex-col gap-[4px]'>
-                        <h3 className='font-semibold text-[#666666] text-lg'>QUICK LINKS</h3>
-                        <FooterLink text={"Privacy Policy"} />
-                        <FooterLink text={"Disclaimer"} />
+                    <div className='flex justify-between px-10 py-2 bg-[#10B5CB] text-sm font-medium text-white'>
+                        <p>Crafted with ❤️ in Punjab, India</p>
+                        <p>© Copyright 2022 by Travelux.ai.  All Rights Reserved</p>
                     </div>
-                    <div className='flex flex-col gap-[4px]'>
-                        <FooterLink text={"Our team"} />
-                        <FooterLink text={"Cancellation Policy"} />
-                        <FooterLink text={"Privacy Policyr"} />
-                    </div>
-                </div>
-                <div className='flex justify-between px-10 py-2 bg-[#10B5CB] text-sm font-medium text-white'>
-                    <p>Crafted with ❤️ in Punjab, India</p>
-                    <p>© Copyright 2022 by Travelux.ai.  All Rights Reserved</p>
                 </div>
             </div>
         </div>
